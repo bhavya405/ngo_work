a32bec8f409325ce21e0f80ab0535a0593c23896
bhavya405 <143187883+bhavya405@users.noreply.github.com>
2026-04-05 14:58:48 -0700

    Ready for deployment

--- src/app/payments/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Input } from "@/components/ui/input";
-import {Elements} from "@stripe/react-stripe-js";
-import {loadStripe} from "@stripe/stripe-js";
-import { use } from "react";
-// import {checkoutModal}
-//import convert to Subcuruency
